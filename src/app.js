4965a529198265a6ecc4b0c03c6076ac43e61542
astrid <nathanmcninch@gmail.com>
2021-07-22 12:29:45 -0700

    add starter code

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const justiceLeague = [
+  { name: "Kara In-Ze", age: 33, alias: "Supergirl" },
+  { name: "Dorian Chase", age: 47, alias: "Vigilante" },
+  { name: "Diana Prince", age: 140, alias: "Wonder Woman" },
+]
+
+//step one create a new feature branch to work from
+
+//step two add this JavaScript file to index.html
+
+//step three add the following functionality:
+// when a name is clicked on a card with the rest of their info is displayed in div .two
+// use only one event listener on div .one (event object and bubbling hint hint)
+// if a second name is clicked the first card disapears and is replaced by a new card 
+// only one card can be displayed at a time.
+// hint .remove()
+// use a template created with a template literal for your new card.
+// hint including a div in your template will help with removing it.
+
+//step four add just enough styling to make your app look a little nicer, without removing any of the existing styling.
